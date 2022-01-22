b01aaba6c0a66b71fd885b36fad21e34fd1a265e
Muhammet Ali AKBAY <me@muhammetaliakbay.com>
2022-01-22 19:32:29 +0100

    Skip saving the tweet already exists

--- database.py
+++ database.py
@@ -7,6 +7,11 @@ cur = con.cursor()
 cur.execute('''CREATE TABLE IF NOT EXISTS tweet (id text PRIMARY KEY, text text)''')
 con.commit()
 
+def has_tweet(id: str):
+    cur.execute('''SELECT id FROM tweet WHERE id = ?''', [id])
+    data = cur.fetchone()
+    return data is not None
+
 def save_tweet(id: str, text: str):
     cur.execute('''INSERT INTO tweet (id, text) VALUES (?, ?)''', (id, text))
     con.commit()
--- process.py
+++ process.py
@@ -1,5 +1,6 @@
 from pytwitter.models import Tweet
-from database import save_tweet
+from database import save_tweet, has_tweet
 
 def process_tweet(tweet: Tweet):
-    save_tweet(id=tweet.id, text=tweet.text)
+    if not has_tweet(id=tweet.id):
+        save_tweet(id=tweet.id, text=tweet.text)
